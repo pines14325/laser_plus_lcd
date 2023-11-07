7288b49bd6e37a6dc1a4ea1c9993ae02ac19c526
James Hoyland <hoyland.jd@gmail.com>
2023-11-06 20:17:50 -0800

    Comments from James

--- laser_plus_lcd.ino
+++ laser_plus_lcd.ino
@@ -20,18 +20,24 @@ double calibrate()
   for(i = 0; i < 100; i++)
   {
     total += 
+      /* Looks like you are planning on taking an average here. So you want to make a new measurement for 
+      each cycle of this loop (the "pulseIn"  needs to be in here) You'll then add each of those measurements to your total.
+      This would be easier if you put the whole of the measurement process inside a separate function. */
   }
   uint64_t a = pulseIn(laserPin,HIGH);
 }
 
 void loop() {
   lcd.clear();
+  // Put all this in a function, from HERE ...
   pinMode(laserPin,OUTPUT);
   digitalWrite(laserPin,HIGH);
   delayMicroseconds(2);
   digitalWrite(laserPin,LOW);
   pinMode(laserPin,INPUT);
   uint64_t time = pulseIn(laserPin,HIGH);
+  // ... to here - you would then return the "time" variable
+  // BTW - don't call it "time" - this name is used in some libraries
       Serial.println(time);
   double distance = time*calibrationFactor;
   buttonState = digitalRead(buttonPin);
